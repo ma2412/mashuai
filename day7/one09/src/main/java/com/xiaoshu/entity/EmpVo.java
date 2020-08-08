e6811ebbec758748e153e54f2f9de15131a8a97a
mashuai <2832233529@qq.com>
2020-08-08 22:45:01 +0800

    第7天作业

---
+++ day7/one09/src/main/java/com/xiaoshu/entity/EmpVo.java
@@ -0,0 +1,26 @@
+package com.xiaoshu.entity;
+
+public class EmpVo extends Emp {
+	private String cname;
+	
+	private Integer sum;
+	
+
+	public Integer getSum() {
+		return sum;
+	}
+
+	public void setSum(Integer sum) {
+		this.sum = sum;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	
+	
+}
