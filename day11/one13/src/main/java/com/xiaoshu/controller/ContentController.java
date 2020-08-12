0d8d916bfb64a3f44c90a808f761698b186d1212
mashuai <2832233529@qq.com>
2020-08-12 21:18:26 +0800

    提交第11天作业

---
+++ day11/one13/src/main/java/com/xiaoshu/controller/ContentController.java
@@ -0,0 +1,196 @@
+package com.xiaoshu.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.ibatis.annotations.Param;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.xiaoshu.config.util.ConfigUtil;
+import com.xiaoshu.entity.Content;
+import com.xiaoshu.entity.Contentcategory;
+import com.xiaoshu.entity.Operation;
+import com.xiaoshu.entity.Role;
+import com.xiaoshu.entity.User;
+import com.xiaoshu.service.ContentService;
+import com.xiaoshu.service.OperationService;
+import com.xiaoshu.service.RoleService;
+import com.xiaoshu.service.UserService;
+import com.xiaoshu.util.StringUtil;
+import com.xiaoshu.util.WriterUtil;
+
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageInfo;
+
+@Controller
+@RequestMapping("content")
+public class ContentController extends LogController{
+	static Logger logger = Logger.getLogger(ContentController.class);
+
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private ContentService contentService;
+	
+	@Autowired
+	private RoleService roleService ;
+	
+	@Autowired
+	private OperationService operationService;
+	
+	
+	@RequestMapping("contentIndex")
+	public String index(HttpServletRequest request,Integer menuid) throws Exception{
+		List<Role> roleList = roleService.findRole(new Role());
+		List<Operation> operationList = operationService.findOperationIdsByMenuid(menuid);
+		List<Contentcategory> ylist = contentService.findy();
+		request.setAttribute("ylist", ylist);
+		request.setAttribute("operationList", operationList);
+		request.setAttribute("roleList", roleList);
+		return "content";
+	}
+	
+	
+	@RequestMapping(value="userList",method=RequestMethod.POST)
+	public void userList(String others,Content content,HttpServletRequest request,HttpServletResponse response,String offset,String limit) throws Exception{
+		try {
+			Integer pageSize = StringUtil.isEmpty(limit)?ConfigUtil.getPageSize():Integer.parseInt(limit);
+			Integer pageNum =  (Integer.parseInt(offset)/pageSize)+1;
+				PageInfo<Content> userList= contentService.findAll(others,pageNum,pageSize);
+				JSONObject jsonObj = new JSONObject();
+				jsonObj.put("total",userList.getTotal() );
+				jsonObj.put("rows", userList.getList());
+		        WriterUtil.write(response,jsonObj.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("用户展示错误",e);
+			throw e;
+		}
+	}
+	
+	
+	// 新增或修改
+	@RequestMapping("reserveUser")
+	public void reserveUser(Content content,MultipartFile Filepicpath,HttpServletRequest request,User user,HttpServletResponse response) throws Exception{
+		Integer userId = content.getContentid();
+		JSONObject result=new JSONObject();
+		if(Filepicpath!=null){
+			//获取图片名称
+			String filename = Filepicpath.getOriginalFilename();
+			//获取后缀名
+			String suffix = filename.substring(filename.lastIndexOf("."));
+			//重新命名
+			String newFileName = System.currentTimeMillis()+suffix;
+			//设置虚拟路径
+			File file = new File("f:/pic/"+newFileName);
+			//上传图片
+			Filepicpath.transferTo(file);
+			//将图片名称保存到数据库中
+			content.setPicpath(newFileName);
+		}
+		try {
+			if (userId != null) {   // userId不为空 说明是修改
+				Content userName = contentService.existUserWithUserName(content.getContenttitle());
+				if(userName != null && userName.getContentid().compareTo(userId)==0){
+					content.setContentid(userId);
+					contentService.updateUser(content);
+					result.put("success", true);
+				}else{
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+				
+			}else {   // 添加
+				if(contentService.existUserWithUserName(content.getContenttitle())==null){  // 没有重复可以添加
+					content.setCreatetime(new Date());
+					contentService.addUser(content);
+					result.put("success", true);
+				} else {
+					result.put("success", true);
+					result.put("errorMsg", "该用户名被使用");
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("保存用户信息错误",e);
+			result.put("success", true);
+			result.put("errorMsg", "对不起，操作失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	
+	@RequestMapping("deleteUser")
+	public void delUser(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			String[] ids=request.getParameter("ids").split(",");
+			for (String id : ids) {
+				contentService.deleteUser(Integer.parseInt(id));
+			}
+			result.put("success", true);
+			result.put("delNums", ids.length);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	@RequestMapping("echarts")
+	public void echarts(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		try {
+			List<Content> clist = contentService.echartlist();
+			result.put("clist", clist);
+			result.put("success", true);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("删除用户信息错误",e);
+			result.put("errorMsg", "对不起，删除失败");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+	
+	@RequestMapping("editPassword")
+	public void editPassword(HttpServletRequest request,HttpServletResponse response){
+		JSONObject result=new JSONObject();
+		String oldpassword = request.getParameter("oldpassword");
+		String newpassword = request.getParameter("newpassword");
+		HttpSession session = request.getSession();
+		User currentUser = (User) session.getAttribute("currentUser");
+		if(currentUser.getPassword().equals(oldpassword)){
+			User user = new User();
+			user.setUserid(currentUser.getUserid());
+			user.setPassword(newpassword);
+			try {
+				userService.updateUser(user);
+				currentUser.setPassword(newpassword);
+				session.removeAttribute("currentUser"); 
+				session.setAttribute("currentUser", currentUser);
+				result.put("success", true);
+			} catch (Exception e) {
+				e.printStackTrace();
+				logger.error("修改密码错误",e);
+				result.put("errorMsg", "对不起，修改密码失败");
+			}
+		}else{
+			logger.error(currentUser.getUsername()+"修改密码时原密码输入错误！");
+			result.put("errorMsg", "对不起，原密码输入错误！");
+		}
+		WriterUtil.write(response, result.toString());
+	}
+}
